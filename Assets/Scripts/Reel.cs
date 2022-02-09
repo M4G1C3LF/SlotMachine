80c2e67055083aecfb1b9964409aa1837bc14e13
M4G1C3LF <sbalaguercarmona@gmail.com>
2022-02-09 23:18:22 +0100

    Create reel motion

---
+++ Assets/Scripts/Reel.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ReelDirection {
+    UPWARDS,
+    DOWNWARDS
+}
+public class Reel : MonoBehaviour
+{
+    [SerializeField]
+    private float spinningSpeed;
+    [SerializeField]
+    private ReelDirection direction;
+    [SerializeField]
+    private float distanceBetweenFigures;
+    private List<GameObject> figures;
+    public bool isSpinning;
+    
+    private void OnEnable() {
+        GetAllFiguresOnReel();
+        SetDistanceBetweenFigures();
+    }
+    private void GetAllFiguresOnReel(){
+        figures = Utils.FindChildrensWithTag(gameObject,Tags.FIGURE);
+    }
+    private void SetDistanceBetweenFigures(){
+        int i = 0;
+        figures.ForEach(figure => {
+            Vector3 actualPosition = figure.transform.localPosition;
+            figure.transform.localPosition = new Vector2(0f,-distanceBetweenFigures*i);
+            i++;
+        });
+    }
+    private void StartSpinning(){
+        isSpinning = true;
+    }
+    private void DoSpinMotion(){
+        Vector3 dir = direction == ReelDirection.UPWARDS ? Vector3.up : Vector3.down;
+
+        figures.ForEach(figure => {
+            if (dir == Vector3.up && HasToTranslateToBottom(figure)){
+                TranslateFigureToBottom(figure);
+                return;    
+            }
+            if (dir == Vector3.down && HasToTranslateToTop(figure)){
+                TranslateFigureToTop(figure);
+                return;    
+            }
+            figure.transform.localPosition += (dir * spinningSpeed * Time.deltaTime);
+        });
+    }
+    private bool HasToTranslateToTop(GameObject figure){
+        return (figure.transform.localPosition.y <= (figures.ToArray().Length) * -distanceBetweenFigures);
+    }
+    private bool HasToTranslateToBottom(GameObject figure){
+        return (figure.transform.localPosition.y >= 0f);
+    }
+    private void TranslateFigureToTop(GameObject figure){
+        float higherPositionFound = float.PositiveInfinity;
+        figures.ForEach(fig => {
+            if (higherPositionFound == float.PositiveInfinity || fig.transform.localPosition.y > higherPositionFound)
+                higherPositionFound = fig.transform.localPosition.y;
+        });
+        figure.transform.localPosition = new Vector2(0f, higherPositionFound + distanceBetweenFigures);
+    }
+    private void TranslateFigureToBottom(GameObject figure){
+        float lowerPositionFound = float.NegativeInfinity;
+        figures.ForEach(fig => {
+            if (lowerPositionFound == float.NegativeInfinity || fig.transform.localPosition.y < lowerPositionFound)
+                lowerPositionFound = fig.transform.localPosition.y;
+        });
+        figure.transform.localPosition = new Vector2(0f, lowerPositionFound - distanceBetweenFigures);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSpinning){
+            DoSpinMotion();
+        }
+    }
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System.IO;
+using UnityEngine.UI;
+
+public static class Utils {
+	/*
+	 *	This method returns a GameObject.
+	 *	It will iterate for each gameObject, if the tag matches, it will return that object.
+	 */
+
+	public static GameObject FindChildrenWithTag(GameObject gO, string tag){
+		Transform[] gameObjects = gO.GetComponentsInChildren<Transform> (true);
+		foreach (Transform child in gameObjects) {
+			if (child.tag == tag) {
+				return child.gameObject;
+			}
+		}
+		return null;
+	}
+    
+	public static List<GameObject> FindChildrensWithTag(GameObject gO, string tag){
+		List<GameObject> foundList = new List<GameObject>();
+		Transform[] gameObjects = gO.GetComponentsInChildren<Transform> (true);
+		foreach (Transform child in gameObjects) {
+			if (child.tag == tag) {
+				foundList.Add(child.gameObject);
+			}
+		}
+		return foundList;
+	}
+    
+}
